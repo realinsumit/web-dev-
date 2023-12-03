f19fd8c5c9608d4d5b0325d4ebeb6db58b67c409
Sumit Kumar Modanwal <krsumitmahewa@gmail.com>
2023-12-03 16:39:16 +0530

    first commit

---
+++ sumit/first_program.py
@@ -0,0 +1,8 @@
+name = input("what is your name")
+print("hello" +name)
+
+
+old_age = input("enter the old age")
+new_age = int(old_age) + 2 
+print(new_age)
+ 
---
+++ web dev js/index.js
@@ -0,0 +1,3 @@
+function showMessage() {
+    alert('Button clicked! Your webpage is working.');
+}
